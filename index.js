b9f3482660a4cff59ae031e9e3bdb94444a22924
Rajkumar V B <vbrajkumar03@gmai.com>
2022-02-28 13:33:21 +0530

    if else and swapping

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-let role = "guest"; 
-switch (role){
+// Conditional Statements in JavaScript : ifelse and switch case
+let hour = 10;
 
-
-    case 'guest' : 
-    console.log("Guest User");
-    break 
-
-    case 'moderator' : 
-    console.log('Moderator User') ;
-
-    default : 
-        console.log('Unknown User') ; 
-
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
 }
+
+// SWAPPING IN JAVASCRIPT
+// let a = "red";
+// let b = "blue";
+// let c = a;
+// a = b;
+// b = c;
+// console.log(a, b);
+
+//SWITCH CASE IN JAVASCRIPT
+// let role = "guest";
+// switch (role) {
+//   case "guest":
+//     console.log("Guest User");
+//     break;
+
+//   case "moderator":
+//     console.log("Moderator User");
+
+//   default:
+//     console.log("Unknown User");
+// }
